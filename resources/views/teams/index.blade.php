c022bf60324854d98fd48466147dc8da3e1717b3
Abbas <abas_235@hotmail.com>
2023-09-21 03:58:17 +0200

    tournaments-page & teamController fixed
    new data table (teams)

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -1,2 +1,12 @@
+@extends('layout')
+
+@section('nav')
+    <nav>
+        <a href="/">Home</a>
+        <a href="/tournaments">Toernooien</a>
+        <a href="/teams" class="active">Teams</a>
+        <a href="/players">Spelers</a>
+    </nav>
+@endsection
 
 <!-- hier komt een lijst van teams -->
